1bc3796ed75f32030c0cf9937e652afb90232303
しゅわりん哨戒班 <github@mail.shuwahina.net>
2024-06-12 02:35:52 +0900

    アビサポ自己紹介シートジェネレーター Version 1.1.0
    [Add] サイトをPWA化
    [Update] アイコンを変更

---
+++ sw.js
@@ -0,0 +1,27 @@
+var CACHE_NAME = 'avispa-supporter-profile-generator';
+var urlsToCache = [
+    '/js/draw.js',
+    '/img/canvas.jpg',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
